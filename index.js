d782480f48299d13dc7deb80d91e092e91206249
Wayer52 <juanjsaavedra52@gmail.com>
2023-08-24 18:27:50 -0500

    Agregue el endpoint del test

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get("/", (req, res) => {
     res.send("hola mundo");
 });
 
+app.get("/test/:nombre", (req, res) => {
+    res.send("Test " + req.params.nombre);
+});
+
 app.listen(port, () => {
     console.log("Puerto escuchando en http://localhost:3000");
 })
