ee6bd8ccd8c70388d116bc089c954ffb1e72885d
Wayer52 <juanjsaavedra52@gmail.com>
2023-08-24 18:19:03 -0500

    Funciona con ngrok

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ app.use(bodyParser.json());
 
 const port = 3000;
 
+app.get("/", (req, res) => {
+    res.send("hola mundo");
+});
+
 app.listen(port, () => {
-    console.log("Puerto escuchando en http:localhost:3000");
+    console.log("Puerto escuchando en http://localhost:3000");
 })
 
 console.log("Hello world");
