61fc6f3deba0f1a2fb36f53780766b1b786c3069
Wayer52 <juanjsaavedra52@gmail.com>
2023-08-26 22:08:23 -0500

    Agregue mi arquitectura aplicando los imports

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-import express from "express"
+import express from "express";
 import bodyParser from "body-parser";
-
+import { routes } from "./network/routes.js";
 const app = express();
+// const router = routes;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = 3000;
-
-app.get("/", (req, res) => {
-    res.send("hola mundo");
-});
-
-app.get("/test/:nombre", (req, res) => {
-    res.send("Test " + req.params.nombre);
-});
+routes(app);
 
 app.listen(port, () => {
     console.log("Puerto escuchando en http://localhost:3000");
-})
+});
 
 console.log("Hello world");
