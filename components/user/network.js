6f1ede741436a1d44d402a8f2b749278e728005d
Wayer52 <juanjsaavedra52@gmail.com>
2023-08-29 18:02:37 -0500

    Agregando el listener.mjs

--- components/user/network.js
+++ components/user/network.js
@@ -17,8 +17,26 @@ router.get('/', (req, res) => {
 });
 
 //------------------Con query params /user?name=xNombre--------------
-router.post('/', (req, res) => {
-    const { name } = req.query; // Lee el query parameter 'name'
+// router.post('/', (req, res) => {
+//     const { name } = req.query; // Lee el query parameter 'name'
+
+//     if (!name) {
+//         response.error(req, res, 'Name parameter is required', 400);
+//         return;
+//     }
+
+//     controller.addUser(name)
+//         .then(data => {
+//             response.success(req, res, data, 201);
+//         })
+//         .catch(e => {
+//             response.error(req, res, 'Internal error', 500, e);
+//         });
+// });
+
+//-------------Con path params /user/xnombre----------------
+router.post('/:name', (req, res) => {
+    const { name } = req.params; // Lee el query parameter 'name'
 
     if (!name) {
         response.error(req, res, 'Name parameter is required', 400);
@@ -34,9 +52,8 @@ router.post('/', (req, res) => {
         });
 });
 
-//-------------Con path params /user/xnombre----------------
-// router.post('/:name', (req, res) => {
-//     const { name } = req.params; // Lee el query parameter 'name'
+// router.post('/', (req, res) => {
+//     const { name } = req.body; // Lee el query parameter 'name'
 
 //     if (!name) {
 //         response.error(req, res, 'Name parameter is required', 400);
--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { routes } from "./network/routes.js";
+import { sendNotification, startNotificationListener } from "./listener.mjs";
+
+
 const app = express();
 // const router = routes;
-
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = 3000;
 routes(app);
+
+startNotificationListener();
+
+app.post("/", (req, res)=> {
+    // console.log(req.body);
+    sendNotification(req.body)
+    res.send("Petición POST")
+})
 
 app.listen(port, () => {
     console.log("Puerto escuchando en http://localhost:3000");
